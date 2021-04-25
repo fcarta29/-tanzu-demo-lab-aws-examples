157c578e3b6bd2d99d8f58fa418d92ce7a0624db
Frank Carta <fcarta@vmware.com>
2021-04-25 11:51:53 -0700

    Checking in temp scripts

---
+++ spring-petclinic/demo/scripts/spring-petclinic-checkin.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+########################
+# include the magic
+########################
+. demo-magic.sh
+
+
+########################
+# Configure the options
+########################
+
+#
+# speed at which to simulate typing. bigger num = faster
+#
+TYPE_SPEED=12
+
+p "git commit -am \"Hello everyone\""
